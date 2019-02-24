d8e9d827437265da84292cac7d93bd8c6a2f1d5b
Dillon Jaap <extradillon@gmail.com>
2019-02-24 13:36:12 -0700

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order("#{sort_column}")
+    @movies = Movie.order("#{sort_column} DESC")
   end
 
   def sort_column
