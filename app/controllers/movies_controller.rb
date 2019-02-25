de9ad202292f35368f9328e256a06e1f8dc3bab9
Dillon Jaap <extradillon@gmail.com>
2019-02-25 01:38:57 -0700

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
 	@sorting_by = nil
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort_by)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort_by, :html)
   end
 
   def hilite_class
